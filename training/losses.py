Fix SRGAN training pipeline: normalize inputs for discriminator and losses, correct content/pixel loss scaling, and detach generator in discriminator step

training/losses.py
@@ -28,8 +28,8 @@ class ContentLoss(tf.keras.losses.Loss):
 
     def call(self, y_true, y_pred):
         # VGG expects inputs in range [0, 255] and preprocessed
-        y_true = tf.clip_by_value(y_true, 0.0, 1.0)
-        y_pred = tf.clip_by_value(y_pred, 0.0, 1.0)
+        # y_true = tf.clip_by_value(y_true, 0.0, 1.0)
+        # y_pred = tf.clip_by_value(y_pred, 0.0, 1.0)
 
         y_true = preprocess_input(y_true * 255.0)
         y_pred = preprocess_input(y_pred * 255.0)
@@ -42,10 +42,7 @@ class ContentLoss(tf.keras.losses.Loss):
 
 
 def pixel_loss(y_true, y_pred):
-    y_pred = tf.clip_by_value(y_pred, 0.0, 1.0)
     return tf.reduce_mean(tf.square(y_true - y_pred))
-
-
 
 
 bce = tf.keras.losses.BinaryCrossentropy(from_logits=False)
@@ -70,8 +67,8 @@ def generator_adversarial_loss(d_fake):
 
 
 def generator_loss(
-    sr,
-    hr,
+    sr, #[0, 255]
+    hr, #[0, 255]
     d_fake,
     content_loss_fn,
     lambda_adv=1e-3,
@@ -89,15 +86,20 @@ def generator_loss(
     - lambda_pixel: weight for pixel loss (optional)
     """
 
-    sr_clipped = tf.clip_by_value(sr, 0.0, 1.0)
+    #normalize from [0, 255] to [0, 1] to content loss
+    hr_norm = hr / 255.0
+    sr_norm = sr / 255.0
 
-    l_content = content_loss_fn(hr, sr_clipped)
+    sr_norm_clip = tf.clip_by_value(sr_norm, 0.0, 1.0)
+    hr_norm_clip = tf.clip_by_value(hr_norm, 0.0, 1.0)
+
+    l_content = content_loss_fn(hr_norm_clip, sr_norm_clip)
     l_adv = generator_adversarial_loss(d_fake)
 
     total_loss = l_content + lambda_adv * l_adv
 
     if lambda_pixel > 0.0:
-        l_pixel = pixel_loss(hr, sr_clipped)
+        l_pixel = pixel_loss(hr_norm_clip, sr_norm_clip)
         total_loss += lambda_pixel * l_pixel
     else:
         l_pixel = tf.constant(0.0)

training/train_srgan.py
@@ -6,8 +6,8 @@ from training.metrics import psnr_metric, ssim_metric
 
 @tf.function
 def train_step_srgan(
-    lr_images,
-    hr_images,
+    lr_images, #[0, 255]
+    hr_images, #[0, 255]
     generator,
     discriminator,
     g_optimizer,
@@ -17,16 +17,17 @@ def train_step_srgan(
     lambda_pixel=0.0,
 ):
     
-    with tf.GradientTape(persistent=True) as tape:
+    with tf.GradientTape() as g_tape, tf.GradientTape() as d_tape:
 
-        sr_images = generator(lr_images, training=True)
+        sr_images = generator(lr_images, training=True) # takes inputs in [0, 255], produces outputs in [0, 255]
 
+        #normalize to [0, 1] for discriminator
+        sr_for_d = sr_images / 255.0
+        hr_for_d = hr_images / 255.0
 
-        d_real = discriminator(hr_images, training=True)
-        d_fake = discriminator(sr_images, training=True)
-
-
-        d_loss = discriminator_loss(d_real, d_fake)
+        #train generator
+        d_real = discriminator(hr_for_d, training=True) # takes inputs in [0, 1], produces outputs in [0, 1]
+        d_fake = discriminator(sr_for_d, training=True)
 
         g_total_loss, g_loss_dict = generator_loss(
             sr=sr_images,
@@ -37,16 +38,20 @@ def train_step_srgan(
             lambda_pixel=lambda_pixel,
         )
 
+        #train discriminator
+        d_fake_detached = discriminator(tf.stop_gradient(sr_for_d), training=True)
+        d_loss = discriminator_loss(d_real, d_fake_detached)
 
-    g_grads = tape.gradient(g_total_loss, generator.trainable_variables)
-    d_grads = tape.gradient(d_loss, discriminator.trainable_variables)
+
+    g_grads = g_tape.gradient(g_total_loss, generator.trainable_variables)
+    d_grads = d_tape.gradient(d_loss, discriminator.trainable_variables)
 
     g_optimizer.apply_gradients(zip(g_grads, generator.trainable_variables))
     d_optimizer.apply_gradients(zip(d_grads, discriminator.trainable_variables))
 
   
-    psnr = psnr_metric(hr_images, sr_images)
-    ssim = ssim_metric(hr_images, sr_images)
+    psnr = psnr_metric(hr_for_d, sr_for_d) # takes inputs in [0, 1]
+    ssim = ssim_metric(hr_for_d, sr_for_d) # takes inputs in [0, 1]
 
     return {
         "g_total": g_total_loss,